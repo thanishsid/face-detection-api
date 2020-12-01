5f228d135de98749169872b9b44c9a5233b041d0
ThanishSid <64691478+ThanishSid@users.noreply.github.com>
2020-12-01 23:10:16 +0530

    fix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const saltRounds = 10;
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   },
 });
